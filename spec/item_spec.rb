51087d61043c3930be29d878b12cba784c41418b
margdaly <margaret.daly@gmail.com>
2023-03-08 09:06:13 -0700

    test: Add #init test to item

---
+++ spec/item_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+RSpec.describe Item do
+  describe '#initialize' do
+    it 'exists and has attributes' do
+      item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+
+      expect(item2).to be_a(Item)
+      expect(item2.name).to eq("Apple Pie (Slice)")
+      expect(item2.price).to eq(2.50)
+    end
+  end
+end
