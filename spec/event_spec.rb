ce51ecd04eca218b22a4e9f5fd669bdf522cebad
margdaly <margaret.daly@gmail.com>
2023-03-08 09:51:49 -0700

    test: Add #food_trucks_that_sell to event

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -11,11 +11,11 @@ RSpec.describe Event do
     @food_truck3 = FoodTruck.new("Palisade Peach Shack")
     @event = Event.new("South Pearl Street Farmers Market")
 
-    @food_truck1.stock(item1, 35)
-    @food_truck1.stock(item2, 7)
-    @food_truck2.stock(item3, 25)
-    @food_truck2.stock(item4, 50)
-    @food_truck3.stock(item1, 65)
+    @food_truck1.stock(@item1, 35)
+    @food_truck1.stock(@item2, 7)
+    @food_truck2.stock(@item3, 25)
+    @food_truck2.stock(@item4, 50)
+    @food_truck3.stock(@item1, 65)
   end
 
   describe '#initialize' do
@@ -34,18 +34,29 @@ RSpec.describe Event do
       @event.add_food_truck(@food_truck1)
       expect(@event.food_trucks).to eq([@food_truck1])
 
-      @event.add_food_truck(food_truck2)
-      @event.add_food_truck(food_truck3)
+      @event.add_food_truck(@food_truck2)
+      @event.add_food_truck(@food_truck3)
       expect(@event.food_trucks).to eq([@food_truck1, @food_truck2, @food_truck3])
     end
 
-    it 'can list all food_trucks names' do
+    it 'can list all #food_trucks_names' do
       @event.add_food_truck(@food_truck1)
       expect(@event.food_trucks_names).to eq(['Rocky Mountain Pies'])
 
-      @event.add_food_truck(food_truck2)
-      @event.add_food_truck(food_truck3)
-      expect(@event.food_trucks_names).to eq(["Rocky Mountain Pies", "Ba-Nom-a-Nom", "Palisade Peach Shack"])      
+      @event.add_food_truck(@food_truck2)
+      @event.add_food_truck(@food_truck3)
+      expect(@event.food_trucks_names).to eq(["Rocky Mountain Pies", "Ba-Nom-a-Nom", "Palisade Peach Shack"])
+    end
+
+    it 'can list #food_trucks_that_sell given item' do
+      @event.add_food_truck(@food_truck1)
+      @event.add_food_truck(@food_truck2)
+      
+      expect(@event.food_trucks_that_sell(@item1)).to eq([@food_truck1])
+      expect(@event.food_trucks_that_sell(@item4)).to eq([@food_truck2])
+      
+      @event.add_food_truck(@food_truck3)
+      expect(@event.food_trucks_that_sell(@item1)).to eq([@food_truck1, @food_truck3])
     end
   end
 end
