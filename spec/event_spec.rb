974e0ac17aaf1b40d44f52a76e53170f270aef6a
margdaly <margaret.daly@gmail.com>
2023-03-08 11:16:15 -0700

    test: Add #total quantity test to event

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -125,4 +125,17 @@ RSpec.describe Event do
       })
     end
   end
+
+  describe '#total_quantity' do
+    it 'can calculate total quantity of an item' do
+      @event.add_food_truck(@food_truck1)
+      expect(@event.total_quantity(@item1)).to eq(35)
+
+      @event.add_food_truck(@food_truck2)
+      @event.add_food_truck(@food_truck3)
+
+      expect(@event.total_quantity(@item4)).to eq(50)
+      expect(@event.total_quantity(@item1)).to eq(100)
+    end
+  end
 end
