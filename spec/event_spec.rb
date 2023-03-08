8947d94011eba63406707b118358768223c88938
margdaly <margaret.daly@gmail.com>
2023-03-08 10:31:06 -0700

    test: Add #overstocked test to event

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -59,4 +59,18 @@ RSpec.describe Event do
       expect(@event.food_trucks_that_sell(@item1)).to eq([@food_truck1, @food_truck3])
     end
   end
+
+  describe '#overstocked_items' do
+    it 'can list items sold by more than 1 food truck AND total quantity greater than 50' do
+      @event.add_food_truck(@food_truck1)
+      expect(@event.overstocked_items).to eq([])
+
+      @event.add_food_truck(@food_truck2)
+      @event.add_food_truck(@food_truck3)
+      expect(@event.overstocked_items).to eq([@item1])
+
+      @food_truck2.stock(@item4, 10)
+      expect(@event.overstocked_items).to eq([@item1, @item4])
+    end
+  end
 end
